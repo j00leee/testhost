7a8b57fd0d88f6a1f6bd0ef8936beb3ae758b53c
julie chung <julie.minjung.chung@gmail.com>
2021-09-20 02:53:41 -0400

    changes

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const express = require('express');
 const app = express();
 
 app.get("/test", (req,res) => {res.send("test")} );
+
+app.listen(5000, () => {
+    console.log("hi it's listening to port 5000");
+});
